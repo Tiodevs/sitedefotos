a5984e21c5bd0512b248707c2be04466e79e9b7d
Tiodevs <Tiodevs>
2024-07-09 14:14:42 -0300

    feat(BD): 3 novos ensaios

--- src/DBfotos.jsx
+++ src/DBfotos.jsx
@@ -1,7 +1,9 @@
 import Capa01 from './assets/Capas/Capa01.jpg'
 import Capa02 from './assets/Capas/Capa2.jpg'
 import Capa03 from './assets/Capas/Capa3.jpg'
-import Capa04 from './assets/Capas/Capa4.jpg'
+import Capa04 from './assets/Capas/Capa4.png'
+import Capa05 from './assets/Capas/Capa5.png'
+import Capa06 from './assets/Capas/Capa6.png'
 
 // Album 01
 import Album0101 from './assets/Ensaio01/01.jpg'
@@ -33,6 +35,51 @@ import Album0302 from './assets/Ensaio03/02.jpg'
 import Album0303 from './assets/Ensaio03/03.jpg'
 import Album0304 from './assets/Ensaio03/04.jpg'
 
+// Album 04
+import Album0401 from './assets/Ensaio04/01.jpg'
+import Album0402 from './assets/Ensaio04/02.jpg'
+import Album0403 from './assets/Ensaio04/03.jpg'
+import Album0404 from './assets/Ensaio04/04.jpg'
+import Album0405 from './assets/Ensaio04/05.jpg'
+import Album0406 from './assets/Ensaio04/06.jpg'
+import Album0407 from './assets/Ensaio04/07.jpg'
+import Album0408 from './assets/Ensaio04/08.jpg'
+import Album0409 from './assets/Ensaio04/09.jpg'
+import Album0410 from './assets/Ensaio04/10.jpg'
+import Album0411 from './assets/Ensaio04/11.jpg'
+import Album0412 from './assets/Ensaio04/12.jpg'
+import Album0413 from './assets/Ensaio04/13.jpg'
+import Album0414 from './assets/Ensaio04/14.jpg'
+
+// Album 05
+import Album0501 from './assets/Ensaio05/01.jpg'
+import Album0502 from './assets/Ensaio05/02.jpg'
+import Album0503 from './assets/Ensaio05/03.jpg'
+import Album0504 from './assets/Ensaio05/04.jpg'
+import Album0505 from './assets/Ensaio05/05.jpg'
+import Album0506 from './assets/Ensaio05/06.jpg'
+import Album0507 from './assets/Ensaio05/07.jpg'
+import Album0508 from './assets/Ensaio05/08.jpg'
+import Album0509 from './assets/Ensaio05/09.jpg'
+import Album0510 from './assets/Ensaio05/10.jpg'
+import Album0511 from './assets/Ensaio05/11.jpg'
+import Album0512 from './assets/Ensaio05/12.jpg'
+import Album0513 from './assets/Ensaio05/13.jpg'
+import Album0514 from './assets/Ensaio05/14.jpg'
+import Album0515 from './assets/Ensaio05/15.jpg'
+import Album0516 from './assets/Ensaio05/16.jpg'
+
+// Album 06
+import Album0601 from './assets/Ensaio06/01.jpg'
+import Album0602 from './assets/Ensaio06/02.jpg'
+import Album0603 from './assets/Ensaio06/03.jpg'
+import Album0604 from './assets/Ensaio06/04.jpg'
+import Album0605 from './assets/Ensaio06/05.jpg'
+import Album0606 from './assets/Ensaio06/06.jpg'
+import Album0607 from './assets/Ensaio06/07.jpg'
+import Album0608 from './assets/Ensaio06/08.jpg'
+import Album0609 from './assets/Ensaio06/09.jpg'
+
 export const DBfotos = [
     {
         id: 1,
@@ -59,6 +106,33 @@ export const DBfotos = [
         subTitle: "Fotos tiradas no templo das sistes da missão Curitiba",
         album: [
             Album0301, Album0302, Album0303, Album0304
+        ]
+    },
+    {
+        id: 4,
+        foto: Capa04,
+        title: "Pré-missão",
+        subTitle: "Fotos tiradas no templo de uma jovem que acabou de receber o seu chamado",
+        album: [
+            Album0401, Album0402, Album0403, Album0404, Album0405, Album0406, Album0407, Album0408, Album0409, Album0410, Album0411, Album0412, Album0413, Album0414
+        ]
+    },
+    {
+        id: 5,
+        foto: Capa05,
+        title: "Evento de acasamento",
+        subTitle: "Fotos tiradas no igreja, um evento lindo e cheio de detalhes delicados",
+        album: [
+            Album0501, Album0502, Album0503, Album0504, Album0505, Album0506, Album0507, Album0508, Album0509, Album0510, Album0511, Album0512, Album0513, Album0514, Album0515, Album0516
+        ]
+    },
+    {
+        id: 6,
+        foto: Capa06,
+        title: "Evento de acasamento",
+        subTitle: "Fotos tiradas no igreja, um evento lindo e cheio de detalhes delicados",
+        album: [
+            Album0601, Album0602, Album0603, Album0604, Album0605, Album0606, Album0607, Album0608, Album0609
         ]
     }
 ];
